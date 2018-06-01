39d2fb8e47bc9788246bf7d18d1784042840f0b6
will kent-daggett <willkentdaggett@gmail.com>
2018-05-31 22:12:15 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_api_gateway_method_response" "ResourceOptions200" {
   status_code = "200"
   response_models = { "application/json" = "Empty" }
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = false,
-    "method.response.header.Access-Control-Allow-Methods" = false,
-    "method.response.header.Access-Control-Allow-Origin" = false
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
   }
 }
